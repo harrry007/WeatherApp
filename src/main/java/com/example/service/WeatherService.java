34e095f37e5b28370aebeed7ecfecb5cc0391cba
Saurabh <ss1998singh@gmail.com>
2021-12-12 16:35:11 +0530

    first commit

---
+++ src/main/java/com/example/service/WeatherService.java
@@ -0,0 +1,123 @@
+package com.example.service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+public class WeatherService {
+	
+	
+	private static HttpURLConnection connection;
+	
+	private static String cityName;
+	private static String unit;
+	private static String pin;
+	double temp;
+	String pressure;
+	String description;
+
+	public static String getCityName() {
+		return cityName;
+	}
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+	public static String getUnit() {
+		return unit;
+	}
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+	public static String getPin() {
+		return pin;
+	}
+	public void setPin(String pin) {
+		this.pin = pin;
+	}
+
+	
+
+
+	static JSONObject getWeather() {
+		BufferedReader reader;
+		String st="https://api.openweathermap.org/data/2.5/weather?q="+getCityName()+"&units="+getUnit()+"&appid=4eba133706e6b63b083f822086e57810";
+		String line;
+		StringBuffer rc=new StringBuffer();
+		
+		try {
+			URL url= new URL(st);
+			connection=(HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setConnectTimeout(5000);
+			connection.setReadTimeout(5000);
+			
+			int status=connection.getResponseCode();
+			if(status>299) {
+				reader=new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+				while((line=reader.readLine())!=null) {
+					rc.append(line);
+				}
+				reader.close();
+				
+			}
+			else {
+				reader=new BufferedReader(new InputStreamReader(connection.getInputStream()));
+				while((line=reader.readLine())!=null) {
+					rc.append(line);
+				}
+				reader.close();
+			}
+			
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+		 JSONObject j=new JSONObject(rc.toString());
+		 return j;
+	}
+	
+	
+	public JSONArray returnWeatherArray() {
+		JSONArray weatherArray=getWeather().getJSONArray("weather");
+		return weatherArray;
+		
+	}
+	
+	
+	public JSONObject returnMain() {
+		JSONObject m=getWeather().getJSONObject("main");
+		temp=m.getDouble("temp");
+		pressure=Integer.toString(m.getInt("pressure"));
+		return m;
+		
+	}
+	
+	public JSONObject returnWind() {
+		JSONObject wind=getWeather().getJSONObject("wind");
+		return wind;
+		
+	}
+	
+	public JSONObject returnSys() {
+		JSONObject sys=getWeather().getJSONObject("sys");
+		return sys;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/main/java/com/example/views/MainView.java
@@ -0,0 +1,253 @@
+package com.example.views;
+
+import java.util.ArrayList;
+
+import javax.swing.GroupLayout.Alignment;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.service.WeatherService;
+import com.vaadin.icons.VaadinIcons;
+import com.vaadin.server.ClassResource;
+import com.vaadin.server.ExternalResource;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.AbstractEmbedded;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Image;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.NativeSelect;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.ValoTheme;
+@SpringUI(path="")
+
+public class MainView extends UI{
+	
+	private WeatherService ws=new WeatherService();
+	VerticalLayout mainLayout;
+	private NativeSelect<String> unitSelect;
+	private TextField textField;
+	private Button searchButton;
+	private HorizontalLayout dashboard;
+	private Label location;
+	private Label currentTemp;
+	private HorizontalLayout mainDescription;
+	private Label weatherDescription;
+	private Label maxWeather;
+	private Label minWeather;
+	private Label humidity;
+	private Label pressure;
+	private Label wind;
+	private ExternalResource iconImg;
+	VerticalLayout pressureLayout;
+	VerticalLayout description;
+	
+	@Override
+	protected void init(VaadinRequest vaadinRequest) {
+		
+		mainLayout();
+		setHeader();
+		setLogo();
+		setForm();
+		dashboard();
+		dashboardDetails();
+		
+		searchButton.addClickListener(clickEvent->{
+        	if(!textField.getValue().equals("")) {
+        		try {
+        			updateUI();
+        			
+        		}catch(JSONException e) {
+        			e.printStackTrace();
+        		}
+        	}
+        	else {
+        		Notification.show("Please Enter city");
+        	}
+        });
+	}
+	
+	private void mainLayout() {
+		mainLayout=new VerticalLayout();
+		mainLayout.setWidth("100%");
+		mainLayout.setSpacing(true);
+		//mainLayout.setMargin(true);
+		setContent(mainLayout);	
+	}
+	
+	private void setHeader() {
+		HorizontalLayout header=new HorizontalLayout();
+		//header.setComponentAlignment(header, com.vaadin.ui.Alignment.MIDDLE_CENTER);
+		Label title= new Label("Weather App");
+		title.addStyleName(ValoTheme.LABEL_H1);
+		title.addStyleName(ValoTheme.LABEL_BOLD);
+		title.addStyleName(ValoTheme.LABEL_COLORED);
+		header.addComponent(title);
+		header.setComponentAlignment(title, com.vaadin.ui.Alignment.MIDDLE_CENTER);
+		mainLayout.addComponent(header);
+		mainLayout.setComponentAlignment(header, com.vaadin.ui.Alignment.MIDDLE_CENTER);
+		
+	}
+	
+	
+	private void setLogo() {
+		HorizontalLayout logo=new HorizontalLayout();
+		
+		Image image=new Image(null,new ClassResource("/static/t.png"));
+		image.setWidth("150px");
+		image.setHeight("150px");
+		logo.addComponent(image);
+		logo.setComponentAlignment(image, com.vaadin.ui.Alignment.MIDDLE_CENTER);
+		mainLayout.addComponent(logo);
+		mainLayout.setComponentAlignment(logo, com.vaadin.ui.Alignment.MIDDLE_CENTER);
+	}
+	
+	private void setForm() {
+		
+		HorizontalLayout form= new HorizontalLayout();
+		form.setSpacing(true);
+		form.setMargin(true);
+		
+		unitSelect=new NativeSelect<>();
+		ArrayList<String> items=new ArrayList<>();
+		items.add("\u00b0"+"C");
+		items.add("\u00b0"+"F");
+		
+		unitSelect.setItems(items);
+		unitSelect.setValue(items.get(0));
+		form.addComponent(unitSelect);
+		
+		
+		textField=new TextField();
+		textField.setWidth("200px");
+		form.addComponent(textField);
+		
+		searchButton=new Button();
+		searchButton.setIcon(VaadinIcons.SEARCH);
+		form.addComponent(searchButton);
+		
+		
+		mainLayout.addComponent(form);
+		mainLayout.setComponentAlignment(form, com.vaadin.ui.Alignment.MIDDLE_CENTER);
+		
+	}
+	
+	
+	private void dashboard() {
+		
+		dashboard=new HorizontalLayout();
+		location=new Label();
+		location.addStyleName(ValoTheme.LABEL_H1);
+		location.addStyleName(ValoTheme.LABEL_LIGHT);
+		
+		currentTemp=new Label();
+		currentTemp.addStyleName(ValoTheme.LABEL_H1);
+		currentTemp.addStyleName(ValoTheme.LABEL_LIGHT);
+		
+		dashboard.addComponent(location);
+		dashboard.addComponent(currentTemp);
+		
+		mainLayout.addComponent(dashboard);
+		mainLayout.setComponentAlignment(dashboard,com.vaadin.ui.Alignment.MIDDLE_CENTER);
+		
+	}
+	
+	private void dashboardDetails() {
+		mainDescription=new HorizontalLayout();
+		
+		
+		description=new VerticalLayout();
+		
+		weatherDescription=new Label();
+		
+		description.addComponent(weatherDescription);
+		
+		maxWeather=new Label();
+		
+		description.addComponent(maxWeather);
+		
+		minWeather=new Label();
+		
+		description.addComponent(minWeather);
+		
+		pressureLayout=new VerticalLayout();
+		
+		pressure=new Label();
+		
+		pressureLayout.addComponent(pressure);
+		
+	    humidity=new Label();
+	    
+	    pressureLayout.addComponent(humidity);
+		
+		wind=new Label();
+		
+	    pressureLayout.addComponent(wind);
+	    
+	    mainDescription.addComponent(description);
+	    mainDescription.addComponent(pressureLayout);
+	    
+	    mainLayout.addComponent(mainDescription);
+	    mainLayout.setComponentAlignment(mainDescription,com.vaadin.ui.Alignment.MIDDLE_CENTER);
+	    
+		
+	}
+	
+	
+	private void updateUI() {
+		String city=textField.getValue();
+		String unit;
+		ws.setCityName(city);
+		if(unitSelect.getValue().equals("F")) {
+			ws.setUnit("imperial");
+			unitSelect.setValue("F");
+			unit="\u00b0"+"F";
+		}
+		else {
+			ws.setUnit("metric");
+			unitSelect.setValue("C");
+			unit="\u00b0"+"C";
+		}
+		JSONObject mainObject=ws.returnMain();
+		int temp=mainObject.getInt("temp");
+		double c=temp;
+		currentTemp.setValue(Double.toString(c)+unit);
+		
+		//getting icon
+		String iconCode=null;
+		String weather=null;
+		JSONArray ja=ws.returnWeatherArray();
+		for(int i=0;i<ja.length();i++) {
+			JSONObject jo=ja.getJSONObject(i);
+			iconCode=jo.getString("icon");
+			weather=jo.getString("description");
+			
+		}
+		//iconImg.setReource(new ExternalResource("https://openweathermap.org/img/wn/"+iconCode+"2x.png"));
+		
+		weatherDescription.setValue("Description : "+weather);
+		minWeather.setValue("Min. Temperature : "+Double.toString(ws.returnMain().getDouble("temp_min"))+unit);
+		maxWeather.setValue("Max. Temperature : "+Double.toString(ws.returnMain().getDouble("temp_max"))+unit);
+		pressure.setValue("Pressure : "+ws.returnMain().getInt("pressure")+" hPa");
+		humidity.setValue("Humidity : "+ws.returnMain().getInt("humidity")+"%");
+		wind.setValue("Wind Speed : "+ws.returnWind().getInt("speed")+"m/s");
+		
+		
+		location.setValue(city.toUpperCase()+" : ");
+		
+		
+		
+		
+		
+	}
+	
+	
+}
